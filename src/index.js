d350ff3fac6ec4c0ad995387d5d4595e6de32b8f
Alan Alencar da Silva <benparvar@users.noreply.github.com>
2026-02-22 15:06:58 -0300

    feat(version) getting plugin version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import { fetchRepoTags, fetchRepoLatest } from './githubTags.js';
+import { getExtensionVersion } from './vscodePluginVersion.js';
 
+const extensionId = "github.copilot-chat";
 const args = process.argv.slice(2);
 let latest = false;
+let pluginVersion = false;
 if (args[0] === '--latest' || args[0] === '-l') {
   latest = true;
+  args.shift();
+} else if (args[0] === '--plugin-version' || args[0] === '-p') {
+  pluginVersion = true;
   args.shift();
 }
 
 const [owner, repo] = args;
-if (!owner || !repo) {
-  console.error('Usage: node src/index.js [--latest] <owner> <repo>');
+if ((!owner || !repo) && !pluginVersion) {
+  console.error('Usage: node src/index.js [--latest|--plugin-version] <owner> <repo>');
   process.exit(1);
 }
 
@@ -22,6 +28,21 @@ if (latest) {
       console.error(err.message);
       process.exit(1);
     });
+} else if (pluginVersion) {
+  console.log(JSON.stringify({ version: pluginVersion }, null, 2));
+
+getExtensionVersion(extensionId)
+  .then(version => {
+    if (version) {
+      console.log(`Versão instalada de ${extensionId}:`, version);
+    } else {
+      console.log("Extensão não encontrada.");
+    }
+  })
+  .catch(err => {
+    console.error("Erro ao obter versão:", err.message);
+  });
+
 } else {
   fetchRepoTags(owner, repo)
     .then(tags => {
